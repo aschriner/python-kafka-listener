0c996ddb7429d0ef8e3909a44a7b2d88bda80a2c
Andy Schriner <andrew@leadgenius.com>
2019-04-09 13:30:16 -0700

    reorg some functions

--- consumer.py
+++ consumer.py
@@ -40,19 +40,6 @@ class BaseConsumer(object):
             self._group_id = self._get_group_id()
         return self._group_id
 
-    def _get_consumer(self):
-        kafka_conf = copy(KAFKA_SETTINGS)
-        kafka_conf.update(
-            {
-                'group.id': self.group_id,
-                'client.id': self.CONSUMER_CLIENT_ID,
-            })
-        consumer = Consumer(**kafka_conf)
-        consumer.subscribe(self.TOPICS)
-        logger.info("Initializing consumer in group '{}' listening on topics {}".format(
-            self.group_id, self.TOPICS))
-        return consumer
-
     def poll(self):
         msg = self.consumer.poll(timeout=1.0)
         if msg is None:
@@ -62,27 +49,13 @@ class BaseConsumer(object):
             if msg.error().code() == KafkaError._PARTITION_EOF:
                 # End of partition event
                 logger.info('{} [{}] reached end at offset {}'.format(
-                    msg.topic(), msg.partition(), msg.offset()
-                ))
-            elif msg.error():
+                    msg.topic(), msg.partition(), msg.offset()))
+            else:
                 # Error
                 raise KafkaException(msg.error())
         else:
             # Proper message
             self.process_message(msg)
-
-    def get_handler(self, msg):
-        payload = json.loads(msg.value())
-        # Make sure to produce messages with an event_type key
-        event_type = payload.get('event_type')
-        if event_type:
-            event_handler = self.EVENT_TYPE_HANDLER_MAPPING.get(event_type)
-            if not event_handler:
-                logger.debug("No handler found for message of type '{}'".format(event_type))
-                raise NoHandlerFoundException()
-        else:
-            event_handler = self.DEFAULT_EVENT_HANDLER
-        return event_handler
 
     def process_message(self, msg):
         logger.debug("Processing message value: {}".format(msg.value()))
@@ -93,12 +66,37 @@ class BaseConsumer(object):
         else:
             handler.process_message(msg)
 
+    def get_handler(self, msg):
+        payload = json.loads(msg.value())
+        # In your producer, make sure to produce messages with an 'event_type' key
+        event_type = payload.get('event_type')
+        if event_type:
+            event_handler = self.EVENT_TYPE_HANDLER_MAPPING.get(event_type)
+            if not event_handler:
+                logger.debug("No handler found for message of type '{}'".format(event_type))
+                raise NoHandlerFoundException()
+        else:
+            event_handler = self.DEFAULT_EVENT_HANDLER
+        return event_handler
+
     def cleanup(self):
         self.consumer.close()  # tis important to commit offsets
+
+    def _get_consumer(self):
+        kafka_conf = copy(KAFKA_SETTINGS)
+        kafka_conf.update({
+            'group.id': self.group_id,
+            'client.id': self.CONSUMER_CLIENT_ID,
+        })
+        consumer = Consumer(**kafka_conf)
+        consumer.subscribe(self.TOPICS)
+        logger.info("Initializing consumer in group '{}' listening on topics {}".format(
+            self.group_id, self.TOPICS))
+        return consumer
 
     def _get_group_id(self):
         consumer_id_suffix = os.environ.get('KAFKA_CONSUMER_GROUP_ID_SUFFIX', self._get_random_id())
         return self.CONSUMER_GROUP_ID + '.' + consumer_id_suffix
 
     def _get_random_id(self):
-        return '%010x' % random.randrange(16 ** 10)
+        return '%10x' % random.randrange(16 ** 10)
